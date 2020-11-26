8ebc0c25b0a3bf3befa3c57611b7f100e2a8f497
95521027 <erfanekramian@gmail.com>
2020-11-26 13:12:57 +0330

    set http request for getting all teachers

--- music-project/src/app/all-teachers/all-teachers.component.ts
+++ music-project/src/app/all-teachers/all-teachers.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-all-teachers',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllTeachersComponent implements OnInit {
   image="default-user.svg"
-  constructor() { }
+  teachers;
+  constructor(private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
+    this.teachers=new Object()
+    this.http.get("https://localhost:44342/Teacher/GetAllTeachers").subscribe(res=>
+  {
+    this.teachers=res
+  })
+  }
+
+  teacherProfile(Id)
+  {
+    localStorage.setItem("categoryId",Id)
+    this.router.navigateByUrl("AdPostsByTypeId")
+    
   }
 
 }
