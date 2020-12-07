3664aa134239f0de8913485edf7c4712cbda4e6e
95521027 <erfanekramian@gmail.com>
2020-12-07 11:55:59 +0330

    show adPost search results using bootstrap cards

--- music-project/src/app/search/search.component.ts
+++ music-project/src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -15,11 +16,14 @@ export class SearchComponent implements OnInit {
   searchTerm=''
   searchFilters;
   searchTermForTeacher=''
+  searchResultAdPosts;
   category=[{id:1,name:"گیتار"},{id:2,name:"سنتور"},{id:3,name:"فلوت"}]
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,protected router:Router) { }
 
   ngOnInit(): void {
     this.searchFilters=new Object()
+    this.searchResultAdPosts = new Object()
+    this.searchResultAdPosts = null
     this.result = new Object()
     this.result=null;
   }
@@ -58,8 +62,14 @@ export class SearchComponent implements OnInit {
     this.searchFilters.types=this.selectedCategory
     this.http.get(this.path+"AdPost/getAdPostsBySearchTerm?searchFilters="+encodeURIComponent(JSON.stringify(this.searchFilters))).subscribe(res=>
       {
-        console.log("result*******: "+res)
+        this.searchResultAdPosts=res
       })
   }
+
+  AdPostClick(Id)
+{
+  localStorage.setItem("AdPostTeacherId",Id)
+  this.router.navigateByUrl("teacher")
+}
 
 }
