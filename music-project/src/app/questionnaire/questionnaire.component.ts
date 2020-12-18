895063c5a629fe461ab605687e45c23de6551dcd
erfanekramian <erfanekramian@gmail.com>
2020-12-18 21:32:43 +0330

    add six questions for students in questionnaire

--- music-project/src/app/questionnaire/questionnaire.component.ts
+++ music-project/src/app/questionnaire/questionnaire.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionnaire.component.css']
 })
 export class QuestionnaireComponent implements OnInit {
+  defaultChoice="guitar"
+  selectedChoice="guitar"
+  type=1
 
   constructor() { }
 
+  
+
   ngOnInit(): void {
+  }
+
+  choose(e)
+  {
+    console.log("*************************");
+    
+    this.selectedChoice = e
+    console.log(e)
+    
   }
 
 }
