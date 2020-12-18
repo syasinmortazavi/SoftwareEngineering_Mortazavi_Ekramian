29e9e5e559b9353e3a05b505c7f575fabc95ad93
95521027 <erfanekramian@gmail.com>
2020-12-18 14:52:28 +0330

    get list of chats for users  from server

--- music-project/src/app/all-teachers/all-teachers.component.ts
+++ music-project/src/app/all-teachers/all-teachers.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MainService } from '../main.service';
 
 @Component({
   selector: 'app-all-teachers',
@@ -10,11 +11,12 @@ import { Router } from '@angular/router';
 export class AllTeachersComponent implements OnInit {
  
   teachers;
-  constructor(private http:HttpClient,private router:Router) { }
+  constructor(private service:MainService,private router:Router) { }
 
   ngOnInit(): void {
     this.teachers=new Object()
-    this.http.get("https://localhost:44342/Teacher/GetAllTeacher").subscribe(res=>
+    // this.http.get("https://localhost:44342/Teacher/GetAllTeacher").subscribe(res=>
+    this.service.http.get(this.service.path+"classroom/teachers/",this.service.httpOptions).subscribe(res=>
   {
     this.teachers=res
   })
@@ -26,6 +28,13 @@ export class AllTeachersComponent implements OnInit {
     localStorage.setItem("AdPostTeacherId",Id)
     this.router.navigateByUrl("teacher")
     
+  }
+
+  goToChat(email,id)
+  {
+    localStorage.setItem("currentChatEmail",email)
+    localStorage.setItem("currentChatId",id)
+    this.router.navigateByUrl("chat")
   }
 
 }
